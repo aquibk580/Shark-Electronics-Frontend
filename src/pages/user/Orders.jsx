5e52774e06a4962c2741cd5ad86f234ee0284596
aquibk580 <aquibk580@gmail.com>
2024-07-26 17:19:55 +0530

    changed orders.jsx

--- src/pages/user/Orders.jsx
+++ src/pages/user/Orders.jsx
@@ -12,9 +12,7 @@ const Orders = () => {
 
   const getOrders = async () => {
     try {
-      const { data } = await axios.get(
-        `/api/v1/order`
-      );
+      const { data } = await axios.get(`/api/v1/order`);
       setOrders(data);
     } catch (error) {
       console.error("Error fetching orders:", error);
@@ -37,53 +35,57 @@ const Orders = () => {
           </div>
           <div className="col-md-9 d-flex flex-column align orders">
             <h2>All Orders</h2>
-            {orders.map((o, i) => {
-              return (
-                <div className="border shadow" key={i}>
-                  <table className="table" id="table">
-                    <thead>
-                      <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">Staus</th>
-                        <th scope="col">Buyer</th>
-                        <th scope="col">Date</th>
-                        <th scope="col">Payment</th>
-                        <th scope="col">Quantity</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      <tr>
-                        <td>{i + 1}</td>
-                        <td>{o?.status}</td>
-                        <td>{o?.buyer?.name}</td>
-                        <td>{moment(o?.createdAt).fromNow()}</td>
-                        <td>{o?.payment.success ? "Success" : "Failed"}</td>
-                        <td>{o?.products.length}</td>
-                      </tr>
-                    </tbody>
-                  </table>
-                  <div className="container">
-                    {o?.products?.map((p) => (
-                      <div className="row mb-3 card flex-row" key={o._id}>
-                        <div className="col-md-4">
-                          <img
-                            src={`/api/v1/product/get-photo/${p._id}`}
-                            className="card-img-top"
-                            alt={p.name}
-                            height="200px"
-                          />
+            {!Array.isArray(orders) || orders.length === 0 ? (
+              <h3>No orders Found</h3>
+            ) : (
+              orders?.map((o, i) => {
+                return (
+                  <div className="border shadow" key={i}>
+                    <table className="table" id="table">
+                      <thead>
+                        <tr>
+                          <th scope="col">#</th>
+                          <th scope="col">Staus</th>
+                          <th scope="col">Buyer</th>
+                          <th scope="col">Date</th>
+                          <th scope="col">Payment</th>
+                          <th scope="col">Quantity</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <tr>
+                          <td>{i + 1}</td>
+                          <td>{o?.status}</td>
+                          <td>{o?.buyer?.name}</td>
+                          <td>{moment(o?.createdAt).fromNow()}</td>
+                          <td>{o?.payment.success ? "Success" : "Failed"}</td>
+                          <td>{o?.products.length}</td>
+                        </tr>
+                      </tbody>
+                    </table>
+                    <div className="container">
+                      {o?.products?.map((p) => (
+                        <div className="row mb-3 card flex-row" key={o._id}>
+                          <div className="col-md-4">
+                            <img
+                              src={`/api/v1/product/get-photo/${p._id}`}
+                              className="card-img-top"
+                              alt={p.name}
+                              height="200px"
+                            />
+                          </div>
+                          <div className="col-md-8 pt-3">
+                            <h5>{p.name}</h5>
+                            <p>{p.description}</p>
+                            <p>Price: ₹{p.price}</p>
+                          </div>
                         </div>
-                        <div className="col-md-8 pt-3">
-                          <h5>{p.name}</h5>
-                          <p>{p.description}</p>
-                          <p>Price: ₹{p.price}</p>
-                        </div>
-                      </div>
-                    ))}
+                      ))}
+                    </div>
                   </div>
-                </div>
-              );
-            })}
+                );
+              })
+            )}
           </div>
         </div>
       </div>
