eda00d0be5e43587920c6d15e2dbfa1a0525b214
aquibk580 <aquibk580@gmail.com>
2024-07-26 18:42:31 +0530

    Fixed header.js

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -22,137 +22,129 @@ const Header = () => {
     toast("Logout Successfully");
   };
   return (
-    <div>
-      <nav
-        className="navbar navbar-expand-lg bg-body-tertiary"
-        style={{ position: "sticky" }}
-      >
-        <div className="container-fluid">
-          <button
-            className="navbar-toggler mx-2"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarTogglerDemo01"
-            aria-controls="navbarTogglerDemo01"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
-            <Link className="navbar-brand">
-              <div className="d-flex">
-                <img src="https://res.cloudinary.com/dpudz7cci/image/upload/v1721992967/logo_se8oz3.webp" height={"50px"} alt="" />
-                <pre>Shark Electronics</pre>
-              </div>
-            </Link>
-            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-              <SearchInput />
-              <li className="nav-item  mx-2">
-                <NavLink
-                  to="/"
-                  className="nav-link nav-link-home"
-                  aria-current="page"
-                >
-                  Home
-                </NavLink>
-              </li>
-              <li className="nav-item dropdown mx-1">
-                <Link
-                  className="nav-link dropdown-toggle"
-                  to={"/categories"}
-                  data-bs-toggle="dropdown"
-                >
-                  Categories
-                </Link>
-                <ul className="dropdown-menu">
-                  <li>
-                    <Link className="dropdown-item" to={"/categories"}>
-                      All Categories
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <div className="container-fluid">
+        <button
+          className="navbar-toggler mx-2"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarTogglerDemo01"
+          aria-controls="navbarTogglerDemo01"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+          <Link className="navbar-brand">
+            <div className="d-flex">
+              <img
+                src="https://res.cloudinary.com/dpudz7cci/image/upload/v1721992967/logo_se8oz3.webp"
+                height={"50px"}
+                alt=""
+              />
+              <pre>Shark Electronics</pre>
+            </div>
+          </Link>
+          <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+            <SearchInput />
+            <li className="nav-item  mx-2">
+              <NavLink
+                to="/"
+                className="nav-link nav-link-home"
+                aria-current="page"
+              >
+                Home
+              </NavLink>
+            </li>
+            <li className="nav-item dropdown mx-1">
+              <Link
+                className="nav-link dropdown-toggle"
+                to={"/categories"}
+                data-bs-toggle="dropdown"
+              >
+                Categories
+              </Link>
+              <ul className="dropdown-menu">
+                <li>
+                  <Link className="dropdown-item" to={"/categories"}>
+                    All Categories
+                  </Link>
+                </li>
+                {categories?.map((c) => (
+                  <li key={c._id}>
+                    <Link className="dropdown-item" to={`/category/${c.slug}`}>
+                      {c.name}
                     </Link>
                   </li>
-                  {categories?.map((c) => (
-                    <li key={c._id}>
-                      <Link
-                        className="dropdown-item"
-                        to={`/category/${c.slug}`}
-                      >
-                        {c.name}
-                      </Link>
-                    </li>
-                  ))}
-                </ul>
-              </li>
+                ))}
+              </ul>
+            </li>
 
-              {!auth.user ? (
-                <>
-                  <li className="nav-item mx-1">
-                    <NavLink
-                      to="/signup"
-                      className="nav-link"
-                      aria-current="page"
-                    >
-                      SignUp
-                    </NavLink>
-                  </li>
-                  <li className="nav-item mx-1">
-                    <NavLink
-                      to="/Login"
-                      className="nav-link"
-                      aria-current="page"
-                    >
-                      Login
-                    </NavLink>
-                  </li>
-                </>
-              ) : (
-                <>
-                  <li className="nav-item dropdown mx-1">
-                    <NavLink
-                      className="nav-link dropdown-toggle"
-                      to=""
-                      role="button"
-                      data-bs-toggle="dropdown"
-                      aria-expanded="false"
-                    >
-                      {auth?.user?.name}
-                    </NavLink>
-                    <ul className="dropdown-menu">
-                      <li>
-                        <NavLink
-                          className="dropdown-item"
-                          to={`/dashboard/${
-                            auth?.user?.role === 1 ? "admin" : "user"
-                          }`}
-                        >
-                          Dashboard
-                        </NavLink>
-                      </li>
-                      <li className="nav-item">
-                        <NavLink
-                          to="/Login"
-                          onClick={handleLogout}
-                          className="dropdown-item"
-                        >
-                          Logout
-                        </NavLink>
-                      </li>
-                    </ul>
-                  </li>
-                </>
-              )}
-              <li className="nav-item cart-text">
-                <Badge count={cart?.items?.length}>
-                  <NavLink to="/cart" className="nav-link" aria-current="page">
-                    Cart
+            {!auth.user ? (
+              <>
+                <li className="nav-item mx-1">
+                  <NavLink
+                    to="/signup"
+                    className="nav-link"
+                    aria-current="page"
+                  >
+                    SignUp
                   </NavLink>
-                </Badge>
-              </li>
-            </ul>
-          </div>
+                </li>
+                <li className="nav-item mx-1">
+                  <NavLink to="/Login" className="nav-link" aria-current="page">
+                    Login
+                  </NavLink>
+                </li>
+              </>
+            ) : (
+              <>
+                <li className="nav-item dropdown mx-1">
+                  <NavLink
+                    className="nav-link dropdown-toggle"
+                    to=""
+                    role="button"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                  >
+                    {auth?.user?.name}
+                  </NavLink>
+                  <ul className="dropdown-menu">
+                    <li>
+                      <NavLink
+                        className="dropdown-item"
+                        to={`/dashboard/${
+                          auth?.user?.role === 1 ? "admin" : "user"
+                        }`}
+                      >
+                        Dashboard
+                      </NavLink>
+                    </li>
+                    <li className="nav-item">
+                      <NavLink
+                        to="/Login"
+                        onClick={handleLogout}
+                        className="dropdown-item"
+                      >
+                        Logout
+                      </NavLink>
+                    </li>
+                  </ul>
+                </li>
+              </>
+            )}
+            <li className="nav-item cart-text">
+              <Badge count={cart?.items?.length}>
+                <NavLink to="/cart" className="nav-link" aria-current="page">
+                  Cart
+                </NavLink>
+              </Badge>
+            </li>
+          </ul>
         </div>
-      </nav>
-    </div>
+      </div>
+    </nav>
   );
 };
 
